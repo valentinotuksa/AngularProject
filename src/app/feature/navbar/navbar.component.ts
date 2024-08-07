0b77d281fb2b3cd8632266aabc85623ea2a24956
valentinotuksa <valentinotuksa@protonmail.com>
2024-08-08 00:55:20 +0200

    finished pagination and refactored scss

--- src/app/feature/navbar/navbar.component.ts
+++ src/app/feature/navbar/navbar.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { heroHome } from '@ng-icons/heroicons/outline';
 
 @Component({
     selector: 'app-navbar',
     standalone: true,
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
-    imports: [RouterModule],
+    imports: [RouterModule, CommonModule, NgIconComponent],
+    viewProviders: [provideIcons({ heroHome })],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+    dropdownVisible = false;
+
+    toggleDropdown(): void {
+        this.dropdownVisible = !this.dropdownVisible;
+    }
+
+    getBarClass(i: number): string[] {
+        return ['bar', this.dropdownVisible ? `bar${i}` : ''];
+    }
+}
--- src/app/page/add/add-page.component.ts
+++ src/app/page/add/add-page.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
