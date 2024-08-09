88969126bb29682071fafae61dab90076da85940
valentinotuksa <valentinotuksa@protonmail.com>
2024-08-09 16:22:42 +0200

    added icons to nav

--- src/app/feature/navbar/navbar.component.ts
+++ src/app/feature/navbar/navbar.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { heroHome } from '@ng-icons/heroicons/outline';
+import { heroCube, heroHome, heroUserPlus } from '@ng-icons/heroicons/outline';
 
 @Component({
     selector: 'app-navbar',
@@ -10,9 +10,13 @@ import { heroHome } from '@ng-icons/heroicons/outline';
     templateUrl: './navbar.component.html',
     styleUrls: ['./navbar.component.scss'],
     imports: [RouterModule, CommonModule, NgIconComponent],
-    viewProviders: [provideIcons({ heroHome })],
+    viewProviders: [provideIcons({ heroHome, heroUserPlus, heroCube })],
 })
 export class NavbarComponent {
+    navLinks = [
+        { path: '/home', label: 'Home', icon: 'heroHome' },
+        { path: '/add', label: 'Add Employee', icon: 'heroUserPlus' },
+    ];
     dropdownVisible = false;
 
     toggleDropdown(): void {
